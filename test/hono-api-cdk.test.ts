d27393086fc72afef6aa88ea2986127b57e56ae3
y-sano <mmyy125115@gmail.com>
2025-01-22 20:07:03 +0900

    Initial commit

---
+++ test/hono-api-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HonoApiCdk from '../lib/hono-api-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hono-api-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HonoApiCdk.HonoApiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
